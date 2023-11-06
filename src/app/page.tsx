dbd420147e7404b4246120a3a5c234b35d30338f
Mark Wong <markwong.digital@gmail.com>
2023-11-06 02:02:14 -0800

    Create hero section for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
 import MaxWidthContainer from "./components/MaxWidthContainer";
+import { ArrowRight } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
 	return (
 		<main>
 			<MaxWidthContainer
 				className="
-					mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center
+					mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center
 				"
 			>
 				<div
@@ -19,6 +22,24 @@ export default function Home() {
 						SaaS Platform is now public!
 					</p>
 				</div>
+				<h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+					Chat with your <span className="text-blue-600">documents</span> in
+					seconds.
+				</h1>
+				<p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
+					This platform allows you to have conversations with any PDF document.
+					Simply upload your file and start asking questions right away.
+				</p>
+				<Link
+					href="/dashboard"
+					target="_blank"
+					className={buttonVariants({
+						size: "lg",
+						className: "mt-5",
+					})}
+				>
+					Get started <ArrowRight className="ml-2 h-5 w-5" />
+				</Link>
 			</MaxWidthContainer>
 		</main>
 	);
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
-import { clsx, ClassValue } from "clsx";
+import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export const getClassNames = (...inputs: ClassValue[]): string => {
+export function getClassNames(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
-};
+}
